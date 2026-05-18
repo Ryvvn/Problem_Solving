4d66460e01bd1fc56e7765d4e308281d6df6b1c5
Tan Nguyen <tannd1008@gmail.com>
2026-05-18 11:09:33 +0700

    solution: add damage amount to damage func()

--- Assets/Lectures/L1000_HealthSystem/L1005_NoDamage/Bullet.cs
+++ Assets/Lectures/L1000_HealthSystem/L1005_NoDamage/Bullet.cs
@@ -16,7 +16,7 @@ namespace CMProblemSolving.L1005_HealthSystem {
 
         private void OnTriggerEnter2D(Collider2D collider2D) {
             if (collider2D.gameObject.TryGetComponent(out Enemy enemy)) {
-                enemy.GetComponent<HealthSystem>().Damage(0);
+                enemy.GetComponent<HealthSystem>().Damage(10);
                 DestroySelf();
             }
         }
